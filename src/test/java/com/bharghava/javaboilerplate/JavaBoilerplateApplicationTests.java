1e922fc01d8b3a14ae7d673d50f99156fa8d6f75
bharghava52 <bhargavaraja12@gmail.com>
2023-09-06 19:16:43 +0530

    [TASK] jacoco plugin added

--- src/test/java/com/bharghava/javaboilerplate/JavaBoilerplateApplicationTests.java
+++ src/test/java/com/bharghava/javaboilerplate/JavaBoilerplateApplicationTests.java
@@ -1,9 +1,10 @@
 package com.bharghava.javaboilerplate;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class JavaBoilerplateApplicationTests {
 
 	@Test

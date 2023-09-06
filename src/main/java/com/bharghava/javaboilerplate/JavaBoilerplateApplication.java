f79a031de51afaf8c09e0fb557fb74948d20f078
bharghava52 <bhargavaraja12@gmail.com>
2023-09-06 18:39:29 +0530

    [TASK] first commit

---
+++ src/main/java/com/bharghava/javaboilerplate/JavaBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.bharghava.javaboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharghava/javaboilerplate/JavaBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharghava.javaboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

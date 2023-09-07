43b6594396cd1606de81b3ee764a82554e6f742d
bharghava52 <bhargavaraja12@gmail.com>
2023-09-07 14:00:29 +0530

    [TASK] added the logback config

--- src/main/java/com/bharghava/javaboilerplate/controller/StudentController.java
+++ src/main/java/com/bharghava/javaboilerplate/controller/StudentController.java
@@ -13,8 +13,11 @@ import com.bharghava.javaboilerplate.entity.Student;
 import com.bharghava.javaboilerplate.model.ResponseData;
 import com.bharghava.javaboilerplate.service.StudentService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/student")
+@Slf4j
 public class StudentController {
 
     @Autowired
@@ -22,18 +25,21 @@ public class StudentController {
 
     @GetMapping("/")
     public ResponseEntity<ResponseData> getAllStudents() {
+        log.info("get All Students api is called");
         return new ResponseEntity<ResponseData>(
             new ResponseData(studentService.getAllStudents()), null, 200);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseData> getbyStudentId(@Param(value = "id") int id) {
+        log.info("get Student by id api is called");
         return new ResponseEntity<ResponseData>(
             new ResponseData(studentService.getById(id)), null, 200);
     }
 
     @PostMapping("/")
     public ResponseEntity<ResponseData> addStudent(@RequestBody Student student){
+        log.info("add Student api is called");
         return new ResponseEntity<ResponseData>(
             new ResponseData(studentService.addStudent(student)), null, 200);
     }
